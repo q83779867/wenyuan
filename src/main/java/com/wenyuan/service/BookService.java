f74d78964796cc32aef4415bd79693c169851399
dijia <83779867@qq.com>
2023-11-13 13:47:57 +0800

    ctrl cv

---
+++ src/main/java/com/wenyuan/service/BookService.java
@@ -0,0 +1,20 @@
+package com.wenyuan.service;
+
+import com.wenyuan.pojo.Books;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface BookService {
+    //增加一本书
+    int addBook(Books books);
+    //删除一本书
+    int deleteBookById(int id);
+    //修改一本书
+    int updateBook(Books books);
+    //查询一本书
+    Books queryBookById(int id);
+    //查询全部的书
+    List<Books> queryAllBooks();
+
+}
---
+++ src/main/java/com/wenyuan/service/BookServiceImpl.java
@@ -0,0 +1,39 @@
+package com.wenyuan.service;
+
+import com.wenyuan.mapper.BookMapper;
+import com.wenyuan.pojo.Books;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public class BookServiceImpl implements BookService{
+
+    //service调 mapper层  组合Mapper
+    private BookMapper bookMapper;
+
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
+    public int addBook(Books books) {
+        return bookMapper.addBook(books);
+    }
+
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+
+    public int updateBook(Books books) {
+        System.out.println("BookServiceImpl : updateBook=>"+books);
+        return bookMapper.updateBook(books);
+    }
+
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    public List<Books> queryAllBooks() {
+        return bookMapper.queryAllBooks();
+    }
+}
